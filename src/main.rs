1a021fa10a7633bc030d80b075018dc414f17d04
FUJII Kazunobu <fujii.kazunobu@gmail.com>
2020-09-26 17:25:03 +0900

    section 2

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let image_width = 256;
+    let image_height = 256;
+
+    print!("P3\n{} {}\n255\n", image_width, image_height);
+
+    for j in (0..image_height).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
+        for i in 0..image_width {
+            let r = i as f32 / (image_width - 1) as f32;
+            let g = j as f32 / (image_height - 1) as f32;
+            let b = 0.25f32;
+
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
+
+            print!("{} {} {}\n", ir, ig, ib);
+        }
+    }
+    eprint!("\nDone.\n");
+}

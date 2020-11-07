56cc3a23f00d6713b12eba66d317d35060c987b1
FUJII Kazunobu <fujii.kazunobu@gmail.com>
2020-11-07 11:49:57 +0900

    section 6.1

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use raytracing::ray::Ray;
 use raytracing::vec3::{Color, Point3, Vec3};
 
-fn hit_sphere(center: Point3, radius: f32, r: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f32, r: &Ray) -> f32 {
     let oc = r.origin() - center;
     let a = Vec3::dot(&r.direction(), &r.direction());
     let b = 2.0 * Vec3::dot(&oc, &r.direction());
     let c = Vec3::dot(&oc, &oc) - radius * radius;
-    b * b - 4.0 * a * c > 0.0
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, &r);
+    if t > 0.0 {
+        let n = Vec3::unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
     let unit_direction = Vec3::unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
